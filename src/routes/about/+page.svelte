8e1c5766f807650db8a6e2ffe03c28bf677ea1fa
Me <thelab@posteo.ch>
2024-02-13 23:01:44 +0100

    foo

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -7,7 +7,7 @@
 	<h1>About this app</h1>
 
 	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
+		This was a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
 		following into your command line and following the prompts:
 	</p>
 
